c1b451248ba37563f76bb0c1b671a98c832943d0
Qkite <yeonjigu4@gmail.com>
2022-10-06 11:48:01 +0900

    url에서 알파벳 뽑아내기

---
+++ src/ListExcer/MapExcercise2.java
@@ -0,0 +1,40 @@
+package ListExcer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExcercise2 {
+    public static void main(String[] args) {
+
+        String repoAddr = "https://github.com/seanee3670/CodeLion/tree/master/src";
+        String repoAddrLower = repoAddr.toLowerCase(); // 소문자 변환
+
+        Map<String, Integer> alphabetCnt = new HashMap<>();
+
+        // 65~90 영어 대문자
+        // 97~122 영어 소문자
+        // map에 글자 먼저 넣기
+
+        for (int i=0; i<26; i++){
+            alphabetCnt.put(Character.toString((char)(i+97)),0);
+        }
+
+
+        //
+        for (int i=0; i<repoAddr.length();i++){
+            String letter = String.valueOf(repoAddrLower.charAt(i));
+            if (alphabetCnt.keySet().contains(letter)){
+                alphabetCnt.put(letter,(alphabetCnt.get(letter) +1));
+            }
+        }
+
+        System.out.println(alphabetCnt.get("b"));
+        System.out.println(alphabetCnt.get("c"));
+        System.out.println(alphabetCnt.get("r"));
+
+
+
+    }
+}
+
+
