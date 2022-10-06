655e414f357246bc6cd2dde7b5d190a2d6bc37bc
Qkite <yeonjigu4@gmail.com>
2022-10-06 09:26:46 +0900

    인터페이스 이용한 calculator-version2

---
+++ src/assignment_1005/Excercise1018.java
@@ -0,0 +1,11 @@
+package assignment_1005;
+
+import java.util.Scanner;
+public class Excercise1018 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String date = scan.nextLine();
+        System.out.println(date);
+    }
+
+}
