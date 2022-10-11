bcbdb4d4f725f1fdb201cc64ff0e31db99a83748
Qkite <yeonjigu4@gmail.com>
2022-10-11 09:17:41 +0900

    programmers 예제 자릿수구하기

---
+++ src/codingtest1011/ProgrammersExample.java
@@ -0,0 +1,30 @@
+package codingtest1011;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+
+        String solutionString = String.valueOf(n);
+
+        int length = solutionString.length();
+
+        int answer = 0;
+
+        for(int i=0; i<length; i++){
+            answer += (int) solutionString.charAt(i);
+        }
+
+        return answer;
+    }
+}
+
+public class ProgrammersExample{
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(987));
+
+    }
+}
