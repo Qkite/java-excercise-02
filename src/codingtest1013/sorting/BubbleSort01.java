425241ff48861e2039356365aae58382cc555cc6
Qkite <yeonjigu4@gmail.com>
2022-10-13 10:09:27 +0900

    버블 정렬 해보기

--- src/codingtest1013/sorting/BubbleSort01.java
+++ src/codingtest1013/sorting/BubbleSort01.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 
 public class BubbleSort01 {
 
-    public int[] sorting(int[] arr){
-        for (int i=0; i<arr.length; i++){
-            for(int j=i+1; j< arr.length; j++){
+    public int[] sorting(int[] arr, int i){
+        for(int j=i+1; j< arr.length; j++){
 
-                if(arr[i]>arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
                 }
 
-            }
         }
+
         return arr;
     }
 
@@ -24,7 +23,7 @@ public class BubbleSort01 {
         int[] arr = new int[]{7,2,3,9,28,11};
         // 오름차순 또는 내림차순으로 정렬하는 알고리즘
 
-        int[] revisedArr= bubbleSort01.sorting(arr);
+        int[] revisedArr= bubbleSort01.sorting(arr, 0);
 
         System.out.println(Arrays.toString(revisedArr));
 
---
+++ src/codingtest1013/sorting/BubbleSort02.java
@@ -0,0 +1,33 @@
+package codingtest1013.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort02 {
+
+    public int[] sorting(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            for(int j=i+1; j< arr.length; j++){
+
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort02 bubbleSort01  = new BubbleSort02();
+        int[] arr = new int[]{7,2,3,9,28,11};
+        // 오름차순 또는 내림차순으로 정렬하는 알고리즘
+
+        int[] revisedArr= bubbleSort01.sorting(arr);
+
+        System.out.println(Arrays.toString(revisedArr));
+
+
+    }
+}
