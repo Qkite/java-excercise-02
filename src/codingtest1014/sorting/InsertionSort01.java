027c4f340be78cb25bfba77e01368c004e591a36
Qkite <yeonjigu4@gmail.com>
2022-10-14 09:32:54 +0900

    삽입 정렬 인스턴스 생성

---
+++ src/codingtest1014/sorting/InsertionSort01.java
@@ -0,0 +1,20 @@
+package codingtest1014.sorting;
+
+public class InsertionSort01 {
+
+    public int[] sort(int[] arr){
+        int i=1;
+
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8,5,6,2,4};
+        InsertionSort01 insertionSort01 = new InsertionSort01();
+        int[] result = insertionSort01.sort(arr);
+
+
+        int i=1;
+    }
+}
