c406abaa3302e15e162fdc21c02652622e511167
Qkite <yeonjigu4@gmail.com>
2022-10-14 09:45:42 +0900

    삽입 정렬 1. 첫번째 반복문 실행하기

--- src/codingtest1014/sorting/InsertionSort01.java
+++ src/codingtest1014/sorting/InsertionSort01.java
@@ -1,12 +1,19 @@
 package codingtest1014.sorting;
 
+import java.util.Arrays;
+
 public class InsertionSort01 {
 
     public int[] sort(int[] arr){
         int i=1;
+        if(arr[i] < arr[i-1]){
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+        }
 
-
-        return new int[]{};
+        return arr;
     }
 
 
@@ -15,7 +22,7 @@ public class InsertionSort01 {
         InsertionSort01 insertionSort01 = new InsertionSort01();
         int[] result = insertionSort01.sort(arr);
 
+        System.out.println(Arrays.toString(result));
 
-        int i=1;
     }
 }
