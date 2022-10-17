0a196ef1f6679ed900196903a8a7dbf216546745
Qkite <yeonjigu4@gmail.com>
2022-10-17 09:20:56 +0900

    알고리즘 - 별찍기 n단, 이중 for문

---
+++ src/codingtest1017/RightTriangle02.java
@@ -0,0 +1,23 @@
+package codingtest1017;
+
+public class RightTriangle02 {
+
+
+    public void writeTriangle(int n){
+        for (int i=0; i<n; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        RightTriangle02 rightTriangle02 = new RightTriangle02();
+
+        rightTriangle02.writeTriangle(10);
+
+
+    }
+}
